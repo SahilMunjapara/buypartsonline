5e44e9a2a45b9579a29a423e33f8ec790b970bbf
SahilMunjapara <sahilmunjapara33@gmail.com>
2022-07-04 16:14:21 +0530

    minor change in strings file

--- lib/UI_Helper/string.dart
+++ lib/UI_Helper/string.dart
@@ -251,15 +251,5 @@ String quantityErrorMsg(String? quantity) {
 dynamic callBackUrlJson = {
   "Data": "0",
   "IsSuccess": true,
-  "Message": "Cart Payment Successfull"
+  "Message": "Cart Payment Successful"
 };
-
-const String contactUsText = '''
-Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
-It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-''';
